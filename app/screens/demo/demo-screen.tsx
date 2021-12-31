de81115c0051addbf4536954ffdccb2a4384821b
U170709005 <almiragurkan@posta.mu.edu.tr>
2021-12-31 18:04:25 +0300

    updated

--- app/screens/demo/demo-screen.tsx
+++ app/screens/demo/demo-screen.tsx
@@ -1,20 +1,21 @@
-import React, { FC } from "react"
-import { Dimensions, /* ImageStyle, */ Text, TextStyle, View, ViewStyle } from "react-native"
+import React, { FC, useState } from "react"
+import { Dimensions, ScrollView, /* ImageStyle, */ Text, TextStyle, View, ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { observer } from "mobx-react-lite"
 import {
   Header,
   Screen,
-/*   AutoImage as Image, */
-  GradientBackground, CreateDigits, CreateOperands, CreateFeatures,
+  /*   AutoImage as Image, */
+  GradientBackground, CustomButton,
 } from "../../components"
 import { NavigatorParamList } from "../../navigators"
 import { color, spacing } from "../../theme"
+
 export const logoIgnite = require("./logo-ignite.png")
-export const logoUpvibe = {uri: 'https://upvibe.net/img/logo2.png'}
+export const logoUpvibe = { uri: "https://upvibe.net/img/logo2.png" }
 
 const sHeight = Dimensions.get("screen").height
-
+const sWidth = Dimensions.get("screen").width
 const FULL: ViewStyle = { flex: 1 }
 const CONTAINER: ViewStyle = {
   backgroundColor: color.transparent,
@@ -25,6 +26,7 @@ const HEADER: TextStyle = {
   paddingTop: spacing[3],
   paddingBottom: spacing[2] - 1,
   paddingHorizontal: 0,
+
 }
 const HEADER_TITLE: TextStyle = {
   ...BOLD,
@@ -32,17 +34,117 @@ const HEADER_TITLE: TextStyle = {
   lineHeight: 15,
   textAlign: "center",
   letterSpacing: 1.5,
+  color: "lightsteelblue"
+}
+const BUTTON: ViewStyle = {
+  flex: undefined,
+  width:'33.33%',
+  height:'25%'
 }
 
 export const DemoScreen: FC<StackScreenProps<NavigatorParamList, "demo">> = observer(
   ({ navigation }) => {
     const goBack = () => navigation.goBack()
-    const n1 = ""
+
+    const [number1, setNumber1] = useState("")
+    const [number2, setNumber2] = useState("")
+    const [operand, setOperand] = useState("")
+    const [display, setDisplay] = useState("")
+    const [displayResult, setDisplayResult] = useState("")
+
+    const operands = ["+" ,"-","*","/"]
+    const numerals = ["1","2","3","4","5","6","7","8","9","0"]
+
+    const updateCalc = (digit) => {
+      setDisplayResult("")
+
+      if (operands.includes(digit)) {
+        if(operand.length<1){
+          setOperand(digit);
+          setDisplay(number1 + digit)
+        }
+      }else if(numerals.includes(digit)){
+          if (operand === '') {
+            setNumber1(number1 + digit)
+            console.log(number1 + digit)
+            setDisplay(number1 + digit)
+          } else {
+            setNumber2(number2 + digit)
+            console.log(number1, " ", operand, " ", number2 + digit)
+            setDisplay(number1 + operand +  number2 + digit)
+          }
+      }else if(digit === '='){
+        console.log(number1, " ", operand, " ", number2, "=")
+        setDisplayResult(chooseAction(operand).toString())
+        console.log(chooseAction(operand))
+        resetRegisters()
+      }else if(digit === 'AC'){
+        setDisplay("")
+        setDisplayResult("")
+      }else if(digit==='C'){
+        if(number2.length>1){
+          setNumber2("")
+        }else if(operand.length>1){
+          setOperand("")
+        }else if(number1.length>1){
+          setNumber1("")
+        }
+      }
+    }
+
+
+    const chooseAction = (operand) => {
+      switch (operand) {
+        case '+':
+          return addition(number1, number2);
+        case '-':
+          return subtraction(number1, number2);
+        case '/':
+          return division(number1, number2);
+        case '*':
+          return multiplication(number1, number2);
+        default:
+          return null;
+      }
+    }
+
+    const addition = (number1, number2) => {
+      return parseFloat(number1) + parseFloat(number2);
+    }
+
+    const subtraction = (number1, number2) => {
+      return parseFloat(number1) - parseFloat(number2)
+    }
+
+    const division = (number1, number2) => {
+      return parseFloat(number1) / parseFloat(number2);
+    }
+
+    const multiplication = (number1, number2) => {
+      return parseFloat(number1) * parseFloat(number2);
+    }
+
+    const resetRegisters = () => {
+      setNumber1("")
+      setNumber2("")
+      setOperand("")
+    }
+
+
+
+    const digits = []
+      for (let i = 1; i < 10; i++) {
+        digits.push(
+          <CustomButton buttonStyles={{flex: undefined, width: "33.33%", height: "25%"}}
+                        key={"btn_" + i.toString()} buttonName={i.toString()}
+                        onPress={() => updateCalc(i.toString())}/>
+        )
+      }
 
 
     return (
       <View testID="DemoScreen" style={FULL}>
-        <GradientBackground colors={["#422443", "#281b34"]} />
+        <GradientBackground colors={["#ffffff", "#ffffff"]} />
         <Screen style={CONTAINER} preset="scroll" backgroundColor={color.transparent}>
           <Header
             headerTx="demoScreen.calculator"
@@ -52,17 +154,38 @@ export const DemoScreen: FC<StackScreenProps<NavigatorParamList, "demo">> = obse
             titleStyle={HEADER_TITLE}
           />
           {/* <Image source={logoUpvibe} style={IGNITE} /> */}
-          <View style={{flex:1, height:sHeight * 0.91}}>
-            <View style={{flex:0.4,flexDirection: "row", backgroundColor: 'white '}}>
-              <Text style={{color:'white', fontSize:50}}>Ekran (calc yazdır) </Text>
-            </View>
-            <View style={{flex:0.6, flexDirection:"column",backgroundColor:'white'}}>
-              <CreateFeatures containerStyle={{flex:0.2}} buttonStyle={{backgroundColor:'lightblue'}}/>
-              <View style={{flex:0.8, flexDirection:"row"}}>
-                <View style={{flex:0.75,flexDirection: "column"}}>
-                  <CreateDigits number1={n1}/>
+          <View style={{ flex: 1, height: sHeight * 0.91 }}>
+            <ScrollView style={{ flex: 0.4, flexDirection: "column", backgroundColor: "white ", }}>
+              <Text style={{ flex:0.75, color: "black", fontSize: 50, width:sWidth*0.95}}>
+                {display}
+              </Text>
+              <Text style={{ flex:0.25, color: "black", fontSize: 50, width:sWidth*0.95}}>
+                {displayResult}
+              </Text>
+            </ScrollView>
+            <View style={{ flex: 0.6, flexDirection: "column", backgroundColor: "white" }}>
+              <View style={{ flex: 0.2, justifyContent: "center", flexDirection: "row" }}>
+                <CustomButton buttonStyles={{ backgroundColor: "lightblue" }} buttonName={"AC"} onPress={() => updateCalc('AC')} />
+                <CustomButton buttonStyles={{ backgroundColor: "lightblue" }} buttonName={"C"} onPress={() => updateCalc('C')}/>
+                <CustomButton buttonStyles={{ backgroundColor: "lightblue" }} buttonName={"+/-"} onPress={() => updateCalc('+/-')}/>
+                <CustomButton buttonStyles={{ backgroundColor: "lightblue" }} buttonName={"%"} onPress={() => updateCalc('%')}/>
+              </View>
+              <View style={{ flex: 0.8, flexDirection: "row" }}>
+                <View style={{ flex: 0.75, flexDirection: "column" }}>
+                  <View style={{flexWrap:"wrap", justifyContent: "center", flexDirection: "row", flex:1}}>
+                    {digits}
+                    <CustomButton buttonStyles={BUTTON} buttonName={"0"} onPress={() => updateCalc('0')}/>
+                    <CustomButton buttonStyles={BUTTON} buttonName={","} onPress={() => updateCalc(',')}/>
+                    <CustomButton buttonStyles={BUTTON} buttonName={"="} onPress={() => updateCalc('=')}/>
+                  </View>
                 </View>
-                <CreateOperands containerStyle={{flex:0.25,flexDirection: "column"}} buttonStyle={{backgroundColor:'lightsteelblue'}}/>
+                <View style={{ flex: 0.25, flexDirection: "column", justifyContent: "center"}}>
+
+                  <CustomButton buttonStyles={{ backgroundColor: "lightsteelblue" }} buttonName={"+"} onPress={() => updateCalc('+')}/>
+                  <CustomButton buttonStyles={{ backgroundColor: "lightsteelblue" }} buttonName={"-"} onPress={() => updateCalc('-')}/>
+                  <CustomButton buttonStyles={{ backgroundColor: "lightsteelblue" }} buttonName={"/"} onPress={() => updateCalc('/')}/>
+                  <CustomButton buttonStyles={{ backgroundColor: "lightsteelblue" }} buttonName={"*"} onPress={() => updateCalc('*')}/>
+                </View>
               </View>
             </View>
           </View>
